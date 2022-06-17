1ed4691e2d58481acdf8cc236b9dfb0952dd6319
Stephon Buckhanan <buckhananstephon@gmail.com>
2022-06-16 19:11:01 -0500

    define variables

--- client.js
+++ client.js
@@ -3,8 +3,15 @@
 //? click listener on button to get inputs and add to table. clear inputs
 $(document).ready(onReady);
 
-let totalMonthly;
+let totalMonthly = 0;
+let firstName = $('.firstName');
+let lastName = $('.lastName');
+let idNumber = $('.idNumber');
+let title = $('.title');
+let annualSalary = $('.annualSalary');
 
 function onReady() {
 	console.log('JQ is ready');
 }
+
+function handleClick() {}
