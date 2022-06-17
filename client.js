0aae50d73283e5615888fb435f9d6db41036368f
Stephon Buckhanan <buckhananstephon@gmail.com>
2022-06-16 19:02:20 -0500

    fix repo

---
+++ client.js
@@ -0,0 +1,10 @@
+//? TODO LIST
+//? get data from inputs with val()
+//? click listener on button to get inputs and add to table. clear inputs
+$(document).ready(onReady);
+
+let totalMonthly;
+
+function onReady() {
+	console.log('JQ is ready');
+}
