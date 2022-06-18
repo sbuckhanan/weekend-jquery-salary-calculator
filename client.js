5fde9796d86363aad921ca629da835d2904475ba
Stephon Buckhanan <buckhananstephon@gmail.com>
2022-06-18 11:57:53 -0500

    start the edit feature

--- client.js
+++ client.js
@@ -13,6 +13,7 @@ function onReady() {
 	$('.submitInfo').on('click', handleClick);
 	//? Click listener for delete button
 	$('.tableBody').on('click', '.deleteButton', handleDelete);
+	$('.tableBody').on('click', '.editButton', handleEdit);
 }
 
 function handleClick() {
@@ -29,12 +30,13 @@ function handleClick() {
 	//? Add val to the DOM
 	$('.tableBody').append(`
 		<tr class="newEmployee">
-			<th>${firstName}</th>
-			<th>${lastName}</th>
-			<th>${idNumber}</th>
-			<th>${title}</th>
+			<th class="thFirstName">${firstName}</th>
+			<th class="thLastName">${lastName}</th>
+			<th class="thIdNumber">${idNumber}</th>
+			<th class="thTitle">${title}</th>
 			<th class="salary">${annualSalary}</th>
 			<th><button type="submit" class="deleteButton">Delete</button></th>
+			<th><button type="submit" class="editButton">Edit</button></th>
 		</tr>
 	`);
 	//? Add annual salary to the total
@@ -64,6 +66,42 @@ function handleDelete() {
 	console.log('Total after subtraction:', totalMonthly);
 	$('.totalMonthly').html(`Total Monthly: ${totalMonthly}`);
 	$(this).parent().parent().remove();
+	colorChange();
+}
+
+function handleEdit() {
+	//? Target parent element of the delete button?
+	//? maybe .remove maybe .clear?
+	console.log('Edit Click');
+	//? get value of current text in these fields
+	let firstName = $(this).parent().parent().children('.thFirstName').text();
+	let lastName = $(this).parent().parent().children('.thLastName').text();
+	let idNumber = $(this).parent().parent().children('.thIdNumber').text();
+	let title = $(this).parent().parent().children('.thTitle').text();
+	let amount = $(this).parent().parent().children('.salary').text();
+	//? append in Input fields with gathered data into table
+	//? append first name row
+	$(this)
+		.parent()
+		.parent()
+		.children('.thFirstName')
+		.html(`<input type="text" class="editFirstName">`);
+	$('.editFirstName').val(`${firstName}`);
+	//? append last name row
+	$(this)
+		.parent()
+		.parent()
+		.children('.thLastName')
+		.html(`<input type="text" class="editLastName">`);
+	$('.editLastName').val(`${lastName}`);
+}
+
+function submitEdit() {
+	//? get the information of those input fields
+	//? .html that info back in the table
+	//? change button back to edit button
+	//? handle change of salary both more and less salary (maybe if logic)
+	//? update new monthly amount
 	colorChange();
 }
 
